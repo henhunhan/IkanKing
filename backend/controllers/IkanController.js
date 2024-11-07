cf67fef405e72e6dbb2fa4f9bec2030596e951c9
HendrichPanjaitan <hendrichpanjaitan10@gmail.com>
2024-11-08 00:47:38 +0700

    page ikan konsumsi dan hias

--- backend/controllers/IkanController.js
+++ backend/controllers/IkanController.js
@@ -21,3 +21,27 @@ exports.getIkanHias = async (req, res) => {
   }
 };
 
+exports.getIkanKonsumsiByCategory = async (req, res) => {
+  const { category } = req.params;
+
+  try {
+    const result = await pool.query(`SELECT * FROM ikankonsumsi WHERE category = $1`, [category]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.getIkanHiasByCategory = async (req, res) => {
+  const { category } = req.params;
+
+  try {
+    const result = await pool.query(`SELECT * FROM ikanhias WHERE category = $1`, [category]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
+
--- backend/routes/ikanroutes.js
+++ backend/routes/ikanroutes.js
@@ -5,6 +5,10 @@ const getIkan = require('../controllers/IkanController');
 // Rute untuk mendapatkan data ikan konsumsi
 router.get('/ikankonsumsi', getIkan.getIkanKonsumsi);
 
+router.get('/ikankonsumsi/:category', getIkan.getIkanKonsumsiByCategory);
+
 router.get('/ikanhias',getIkan.getIkanHias );
+
+router.get('/ikanhias/:category', getIkan.getIkanHiasByCategory);
 
 module.exports = router;
