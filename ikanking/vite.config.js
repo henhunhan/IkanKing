6e695641d939b7f219f36badba96af04e233d183
HendrichPanjaitan <hendrichpanjaitan10@gmail.com>
2025-01-28 19:02:53 +0700

    last push (maybe)

--- ikanking/vite.config.js
+++ ikanking/vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview:{
-    port: 3000,
-    strictPort: true,
-  },
   server: {
-    port: 3000, 
-    strictPort: true,
+    port: 3000, // Port server
     host: true,
-    origin: "http://localhost:3000",
+    strictPort: true, // Gunakan port ini atau gagal
+    origin: 'https://lai24b-k10.tekomits.my.id', // Alamat frontend yang diakses browser
     proxy: {
-      '/api': 'http://localhost:5000/api',
-      "rules": {
-}
+      '/api': {
+        target: 'http://localhost:5050', // Alamat backend
+        changeOrigin: true,
+      },
     },
-    
+    hmr: {
+      host: 'lai24b-k10.tekomits.my.id', // Sesuaikan dengan host yang dapat diakses oleh browser
+      port: 3000, // Gunakan port server
+    },
   },
-})
+});
